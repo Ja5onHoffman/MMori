f6c308691f0b2880c2923802655f779fd7e45ca7
Ja5onHoffman <jasonandrewhoffman@gmail.com>
2013-05-11 08:09:19 -0400

    first commit

---
+++ mori.js
@@ -0,0 +1,52 @@
+var birthday = new Date();
+var now = new Date();
+var today = now.getTime();
+var timeDiff = 0;
+
+
+$(document).ready(function(){
+  $('#send').on('click', getDate);
+  $('#addbutton').on('click', defineTable);
+});
+
+function addRow() {
+  $('#daytable').append('<tr><td></td><td></td><td></td><td></td><td></td></tr>');
+}
+             
+function toggle() {
+ $(this).toggleClass('active');
+ }
+
+function defineTable() {
+  getDate();
+  var rows = Number($('#yearsalive').val());
+  createTable(rows);
+  $('#daytable td:lt('+timeDiff+')').addClass('active');
+}
+
+function createTable(rows) {
+  var tbl = document.getElementById('daytable');
+    for (var i = 0; i < rows; i++) {
+      var row = document.createElement('tr');
+        for (var j = 0; j < 52; j++) {
+          var cell = document.createElement('td');
+            row.appendChild(cell);
+        }
+        tbl.appendChild(row);
+    }
+}
+
+function getDate() {
+  var month = Number($('#month').val() - 1);
+  birthday.setMonth(month);
+  var day = Number($('#day').val());
+  birthday.setDate(day);
+  var year = Number($('#year').val());
+  birthday.setFullYear(year);
+  $('#month').val('');
+  $('#day').val('');
+  $('#year').val('');
+  timeDiff += Math.floor((now - birthday) / 604800000);
+}
+
+
